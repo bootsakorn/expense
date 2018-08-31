5c4790e481aa6c5536f78b8a33735e6c8786c97a
bootsakorn <bootsakorn.p@ku.th>
2018-08-31 14:23:02 +0700

    add ListExpense class commit

--- src/main/java/csku/expense/Expense.java
+++ src/main/java/csku/expense/Expense.java
@@ -4,4 +4,5 @@ package csku.expense;
  * Created by 708 on 8/31/2018.
  */
 public class Expense {
+
 }
---
+++ src/main/java/csku/expense/ListExpense.java
@@ -0,0 +1,16 @@
+package csku.expense;
+
+/**
+ * Created by 708 on 8/31/2018.
+ */
+public class ListExpense {
+    public String type, detail;
+    public double amount;
+    public ListExpense (String type, String detail, double amount){
+        this.type = type;
+        this.detail = detail;
+        this.amount = amount;
+    }
+
+
+}
