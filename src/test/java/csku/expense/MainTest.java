dabf62cd39f8d7b84f72d58205f3c0fcd9d728ef
708 <bootsakorn.p@ku.th>
2018-09-03 07:38:09 +0700

    add test and main class commit

---
+++ src/test/java/csku/expense/MainTest.java
@@ -0,0 +1,16 @@
+package csku.expense;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by 708 on 9/3/2018.
+ */
+class MainTest extends TestCase {
+    @Test
+    void main() {
+    }
+
+}
